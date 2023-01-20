768525626cc7bd3598eea04013cfd49173458bd6
Morgan Chang <shin19991207@gmail.com>
2023-01-20 12:28:58 -0500

    Address peer review comments (#6)
    * update testApp.bat
    
    * Update testApp.sh
    
    * update testApp.bat

--- finish/scripts/testApp.bat
+++ finish/scripts/testApp.bat
@@ -1,11 +1,19 @@
-mvn -pl system -ntp clean package liberty:create liberty:install-feature liberty:deploy
-mvn -pl query -ntp clean package liberty:create liberty:install-feature liberty:deploy
+call mvn -pl system ^
+         -ntp clean package liberty:create liberty:install-feature liberty:deploy
+call mvn -pl query ^
+         -ntp clean package liberty:create liberty:install-feature liberty:deploy
 
-mvn -pl system -ntp -P testing liberty:start
-mvn -pl query -ntp liberty:start
+call mvn -pl system ^
+         -ntp -P testing liberty:start
+call mvn -pl query ^
+         -ntp -Dliberty.var.mp.config.profile="testing" liberty:start
 
-mvn -pl system -ntp -P testing failsafe:integration-test
-mvn -pl query -ntp failsafe:integration-test
+call mvn -pl system ^
+         -ntp -P testing failsafe:integration-test
+call mvn -pl query ^
+         -ntp failsafe:integration-test
 
-mvn -pl query -ntp liberty:stop
-mvn -pl system -ntp liberty:stop
+call mvn -pl query ^
+         -ntp liberty:stop
+call mvn -pl system ^
+         -ntp liberty:stop
