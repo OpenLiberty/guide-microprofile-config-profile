93bc587ccd3fb99d890d53c563aa3289f9c423eb
Gilbert Kwan <gkwan@ca.ibm.com>
2023-03-14 15:36:11 -0400

    Dev content review (#15)
    * dev content review
    
    * dev content review
    
    * dev content review
    
    * dev content review
    
    * dev content review
    
    * dev content review
    
    * dev content review
    
    * Update README.adoc
    
    Co-authored-by: David Mueller <48686014+dmuelle@users.noreply.github.com>
    
    * Update README.adoc
    
    Co-authored-by: David Mueller <48686014+dmuelle@users.noreply.github.com>
    
    * Update README.adoc
    
    Co-authored-by: David Mueller <48686014+dmuelle@users.noreply.github.com>
    
    * Update README.adoc
    
    Co-authored-by: David Mueller <48686014+dmuelle@users.noreply.github.com>
    
    * Update README.adoc
    
    * explain more for the testApp.sh file
    
    * explain more for the testApp.sh file
    
    * explain more for the testApp.sh file
    
    * explain more for the testApp.sh file
    
    * explain more for the testApp.sh file
    
    * explain more for the testApp.sh file
    
    * explain more for the testApp.sh file
    
    * Update README.adoc
    
    ---------
    
    Co-authored-by: David Mueller <48686014+dmuelle@users.noreply.github.com>

--- finish/scripts/testApp.sh
+++ finish/scripts/testApp.sh
@@ -1,11 +1,17 @@
+#!/bin/bash
+
 # tag::build[]
 mvn -pl system -ntp clean package liberty:create liberty:install-feature liberty:deploy
 mvn -pl query -ntp clean package liberty:create liberty:install-feature liberty:deploy
 # end::build[]
 
 # tag::start[]
+# tag::start-system[]
 mvn -pl system -ntp -P testing liberty:start
+# end::start-system[]
+# tag::start-query[]
 mvn -pl query -ntp -Dliberty.var.mp.config.profile="testing" liberty:start
+# end::start-query[]
 # end::start[]
 
 # tag::test[]
