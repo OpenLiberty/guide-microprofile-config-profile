a2a28dc64dc04629c593040b351cad0034321891
Morgan Chang <shin19991207@gmail.com>
2023-09-06 07:50:08 -0400

    update README

--- finish/scripts/testApp.sh
+++ finish/scripts/testApp.sh
@@ -7,15 +7,15 @@ mvn -pl query -ntp clean package liberty:create liberty:install-feature liberty:
 
 # tag::start[]
 # tag::start-system[]
-mvn -pl system -ntp -P testing liberty:start
+mvn -pl system -ntp -P test liberty:start
 # end::start-system[]
 # tag::start-query[]
-mvn -pl query -ntp -Dliberty.var.mp.config.profile="testing" liberty:start
+mvn -pl query -ntp -Dliberty.var.mp.config.profile="test" liberty:start
 # end::start-query[]
 # end::start[]
 
 # tag::test[]
-mvn -pl system -ntp -P testing failsafe:integration-test
+mvn -pl system -ntp -P test failsafe:integration-test
 mvn -pl query -ntp failsafe:integration-test
 # end::test[]
 
