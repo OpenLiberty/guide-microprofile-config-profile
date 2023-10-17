5e0cefcbcd8cda94442fdf9006f4f8d4ff999491
Gilbert Kwan <gkwan@ca.ibm.com>
2023-10-17 10:03:09 -0400

    Update README.adoc (#22)
    * Update README.adoc
    
    * Update testApp.sh
    
    * dev content review

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -13,33 +13,36 @@ mvn -ntp -Dhttp.keepAlive=false \
 
 # Testing the testing environment
 
-mvn -pl system -ntp -P testing liberty:start
-mvn -pl query -ntp -Dliberty.var.mp.config.profile="testing" liberty:start
+mvn -pl system -ntp -P test liberty:start
+mvn -pl query -ntp -Dliberty.var.mp.config.profile="test" liberty:start
 
-mvn -pl system -ntp -P testing failsafe:integration-test
+mvn -pl system -ntp -P test failsafe:integration-test
 mvn -pl query -ntp failsafe:integration-test
+mvn -ntp failsafe:verify
 
 mvn -pl system -ntp liberty:stop
 mvn -pl query -ntp liberty:stop
 
 # Testing the development environment
 
-mvn -pl system -ntp -P development liberty:start 
-mvn -pl query -ntp -Dliberty.var.mp.config.profile="development" liberty:start 
+mvn -pl system -ntp -P dev liberty:start 
+mvn -pl query -ntp -Dliberty.var.mp.config.profile="dev" liberty:start 
 
-mvn -pl system -ntp -P development failsafe:integration-test
+mvn -pl system -ntp -P dev failsafe:integration-test
 mvn -pl query -ntp failsafe:integration-test
+mvn -ntp failsafe:verify
 
 mvn -pl system -ntp liberty:stop
 mvn -pl query -ntp liberty:stop
 
 # Testing the production environment
 
-mvn -pl system -ntp -P production liberty:start
-mvn -pl query -ntp -Dliberty.var.mp.config.profile="production" liberty:start
+mvn -pl system -ntp -P prod liberty:start
+mvn -pl query -ntp -Dliberty.var.mp.config.profile="prod" liberty:start
 
-mvn -pl system -ntp -P production failsafe:integration-test
+mvn -pl system -ntp -P prod failsafe:integration-test
 mvn -pl query -ntp failsafe:integration-test
+mvn -ntp failsafe:verify
 
 mvn -pl system -ntp liberty:stop
 mvn -pl query -ntp liberty:stop
