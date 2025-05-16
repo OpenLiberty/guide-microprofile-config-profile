7dfe0b76c6421a38a59a663f4aa770b0b97252ac
Siaa Gor <Siaa.Gor@ibm.com>
2025-05-16 13:30:16 -0400

    update maven wrapper
    Signed-off-by: Siaa Gor <Siaa.Gor@ibm.com>

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -1,55 +1,56 @@
+./mvnw -version
 #!/bin/bash
 set -euxo pipefail
 
-mvn -ntp -Dhttp.keepAlive=false \
+./mvnw -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -pl system -q clean package liberty:create liberty:install-feature liberty:deploy
 
-mvn -ntp -Dhttp.keepAlive=false \
+./mvnw -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -pl query -q clean package liberty:create liberty:install-feature liberty:deploy
 
 # Testing the testing environment
 
-mvn -pl system -ntp -P test liberty:start
-mvn -pl query -ntp -Dliberty.var.mp.config.profile="test" liberty:start
+./mvnw -pl system -ntp -P test liberty:start
+./mvnw -pl query -ntp -Dliberty.var.mp.config.profile="test" liberty:start
 
-mvn -pl system -ntp -P test failsafe:integration-test
-mvn -pl query -ntp failsafe:integration-test
-mvn -ntp failsafe:verify
+./mvnw -pl system -ntp -P test failsafe:integration-test
+./mvnw -pl query -ntp failsafe:integration-test
+./mvnw -ntp failsafe:verify
 
-mvn -pl system -ntp liberty:stop
-mvn -pl query -ntp liberty:stop
+./mvnw -pl system -ntp liberty:stop
+./mvnw -pl query -ntp liberty:stop
 
 # Testing the development environment
 
-mvn -pl system -ntp -P dev liberty:start 
-mvn -pl query -ntp -Dliberty.var.mp.config.profile="dev" liberty:start 
+./mvnw -pl system -ntp -P dev liberty:start 
+./mvnw -pl query -ntp -Dliberty.var.mp.config.profile="dev" liberty:start 
 
-mvn -pl system -ntp -P dev failsafe:integration-test
-mvn -pl query -ntp failsafe:integration-test
-mvn -ntp failsafe:verify
+./mvnw -pl system -ntp -P dev failsafe:integration-test
+./mvnw -pl query -ntp failsafe:integration-test
+./mvnw -ntp failsafe:verify
 
-mvn -pl system -ntp liberty:stop
-mvn -pl query -ntp liberty:stop
+./mvnw -pl system -ntp liberty:stop
+./mvnw -pl query -ntp liberty:stop
 
 # Testing the production environment
 
-mvn -pl system -ntp -P prod liberty:start
-mvn -pl query -ntp -Dliberty.var.mp.config.profile="prod" liberty:start
+./mvnw -pl system -ntp -P prod liberty:start
+./mvnw -pl query -ntp -Dliberty.var.mp.config.profile="prod" liberty:start
 
-mvn -pl system -ntp -P prod failsafe:integration-test
-mvn -pl query -ntp failsafe:integration-test
-mvn -ntp failsafe:verify
+./mvnw -pl system -ntp -P prod failsafe:integration-test
+./mvnw -pl query -ntp failsafe:integration-test
+./mvnw -ntp failsafe:verify
 
-mvn -pl system -ntp liberty:stop
-mvn -pl query -ntp liberty:stop
+./mvnw -pl system -ntp liberty:stop
+./mvnw -pl query -ntp liberty:stop
 
 # Testing the docker environment
 
-mvn -P prod package
+./mvnw -P prod package
 docker build -t system:1.0-SNAPSHOT system/.
 docker build -t query:1.0-SNAPSHOT query/.
 
